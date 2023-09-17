849f5e6652481aaace45f2fbb459e18c59b16f2d
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2023-09-17 17:00:06 +0700

    Add representatives data

---
+++ representatives/main.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+import html
+import json
+
+url = 'https://hris.parliament.go.th/ss_th.php'
+
+data = []
+
+try:
+    response = requests.get(url)
+    if response.status_code == 200:
+        html_content = response.content.decode('utf-8', errors='ignore')
+
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        li_elements = soup.find_all('li')
+
+        for li in li_elements:
+            name_element = li.find('a', class_='sl_name')
+            id_element = li.find('span', class_='label label-info')
+
+            name = name_element.text.strip()
+            idNum = id_element.text.strip()
+            decoded_name = html.unescape(name)
+
+            img_element = li.find('img')
+            image_url = img_element['src']
+
+            data.append({
+                "ID": idNum.split()[2],
+                "Name": name,
+                "Image": image_url
+            })
+
+        try:
+            with open('./representatives/data.json', 'w', encoding='utf-8') as output_file:
+                json.dump(data, output_file, ensure_ascii=False, indent=4)
+            
+            print('File saved')
+        except Exception as error:
+            print(error)
+
+    else:
+        print(f"Failed to fetch HTML source code. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {str(e)}")
