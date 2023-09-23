afc88f2091f5e88c53780b92e92c2ba755cb1d1e
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2023-09-23 16:01:54 +0700

    add API

---
+++ API/main.py
@@ -0,0 +1,38 @@
+from typing import Union
+from fastapi import FastAPI, Query, HTTPException
+import json
+import urllib.parse
+
+app = FastAPI()
+
+def thaiToUnicode(text):
+    # Convert Thai text to Unicode escape sequences
+    unicode_text = ''.join([f'\\u{ord(char):04x}' for char in text])
+    return unicode_text
+
+
+with open("../data/representatives.json") as members:
+    fileContent = json.load(members)
+
+@app.get("/representatives")
+def read_root(ID: Union[str, None] = Query(None, description="Representative ID filter"),
+               Party: Union[str, None] = Query(None, description="Representative party filter (Thai)")):
+    if ID:
+        filteredMemberID = [member for member in fileContent if member.get("ID") == ID]
+        if not filteredMemberID:
+            raise HTTPException(status_code=404, detail="Member not found")
+        return filteredMemberID
+    elif Party:
+        print(f"Searching for party: {Party}")
+        unicodePartyName = thaiToUnicode(Party)  # Strip leading/trailing spaces]
+        print(unicodePartyName)
+        filteredMemberParty = [member for member in fileContent if
+                               thaiToUnicode(member.get("Party", "")).strip() == unicodePartyName]
+        print(f"Found {len(filteredMemberParty)} members.")
+        if not filteredMemberParty:
+            raise HTTPException(status_code=404, detail="Members not found")
+        return filteredMemberParty
+    return fileContent
+
+
+
