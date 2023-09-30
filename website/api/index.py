280bd8a58036dc092fa5fbe17a19541eafa93d59
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2023-09-30 23:01:53 +0700

    Add fileter and categorise api docs

--- website/api/index.py
+++ website/api/index.py
@@ -2,18 +2,6 @@ from typing import Union
 from fastapi import FastAPI, Query, HTTPException
 import requests
 
-description = """
-
-## Members
-
-List of the member of the parliament, including representatives and senators
-
-## Bills
-
-Tracking bills in the parliament
-
-"""
-
 tagsMetadata = [
     {
         "name": "Members",
@@ -30,7 +18,6 @@ app = FastAPI(
     redoc_url=None,
     openapi_url="/api/openapi.json",
     title="Parliament Tracker API",
-    description=description,
     summary="All APIs about Thailand's Parliament",
     version="0.0.1",
 )
@@ -41,29 +28,22 @@ def thaiToUnicode(text):
     return unicode_text
 
 
+
 representativesUrl = 'https://raw.githubusercontent.com/ronnapatp/ParliamentTracker/main/data/representatives.json'
 representativesResponse = requests.get(representativesUrl)
 representativesContent = representativesResponse.json()
 
-@app.get("/api/representatives", tags=["Members"])
-def read_root(ID: Union[str, None] = Query(None, description="Representative ID filter"),
-               Party: Union[str, None] = Query(None, description="Representative party filter (Thai)")):
-    if ID:
-        filteredMemberID = [member for member in representativesContent if member.get("ID") == ID]
-        if not filteredMemberID:
-            raise HTTPException(status_code=404, detail="Member not found")
-        return filteredMemberID
-    elif Party:
-        print(f"Searching for party: {Party}")
-        unicodePartyName = thaiToUnicode(Party)  
-        print(unicodePartyName)
-        filteredMemberParty = [member for member in representativesContent if
-                               thaiToUnicode(member.get("Party", "")).strip() == unicodePartyName]
-        print(f"Found {len(filteredMemberParty)} members.")
-        if not filteredMemberParty:
-            raise HTTPException(status_code=404, detail="Members not found")
-        return filteredMemberParty
+@app.get("/api/representatives", tags=["Members"], name="Return a list of lower house members")
+def read_root():
     return representativesContent
+
+@app.get("/api/representatives/{id}", tags=["Members"], name="Return member by ID")
+async def read_item(id: str):
+    item = next((item for item in representativesContent if item["id"] == id), None)
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
+
 
 billsUrl = 'https://raw.githubusercontent.com/ronnapatp/ParliamentTracker/main/data/bills.json'
 billsResponse = requests.get(billsUrl)
